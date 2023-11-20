dcf901e1ecafd2e33dae5195530b86574e07c0fc
josevilanir <104863215+josevilanir@users.noreply.github.com>
2023-11-20 06:37:19 -0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,23 @@
+const soap = require('soap');
+
+const url = 'http://localhost:8000/?wsdl';
+
+soap.createClient(url, (err, client) => {
+    if (err) {
+        console.error(err);
+    } else {
+        // Chama o método remoto do serviço
+        const params = {
+            num1: 5.0,
+            num2: 7.0
+        };
+
+        client.calcularMedia(params, (err, result) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Média calculada:', result);
+            }
+        });
+    }
+});
