95eff2d498a8a74dfb2f659f7789b0ab8b469ce4
josevilanir <104863215+josevilanir@users.noreply.github.com>
2023-11-20 06:40:12 -0300

    Update soap_server.py

--- soap_server.py
+++ soap_server.py
@@ -1,20 +1,24 @@
-from spyne import Application, rpc, ServiceBase, Integer, Unicode
+from spyne import Application, rpc, ServiceBase, Integer, Float
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 
 class MediaService(ServiceBase):
-    @rpc(Integer, _returns=Unicode)
-    def calcular_media(ctx, *numeros):
-        media = sum(numeros) / len(numeros)
-        return f'A média é: {media}'
+    @rpc(Float, Float, _returns=Float)
+    def calcular_media(ctx, num1, num2):
+        media = (num1 + num2) / 2
+        return media
 
-application = Application([MediaService], 'urn:media', in_protocol=Soap11(validator='lxml'), out_protocol=Soap11())
+# Criação da aplicação SOAP
+app = Application([MediaService], 'exemplo_soap',
+                  in_protocol=Soap11(validator='lxml'),
+                  out_protocol=Soap11())
+
+# Configuração do servidor WSGI
+wsgi_app = WsgiApplication(app)
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
 
-    wsgi_app = WsgiApplication(application)
+    # Inicializa o servidor na porta 8000
     server = make_server('0.0.0.0', 8000, wsgi_app)
-
-    print("Servidor SOAP iniciado. Aguardando requisições...")
     server.serve_forever()
